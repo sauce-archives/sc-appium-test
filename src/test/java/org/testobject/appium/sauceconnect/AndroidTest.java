17d8c9cd6c047d7f2e0621f3da6703df47654eef
crummy <crummynz@gmail.com>
2017-09-14 10:15:08 +0200

    Add an Android test

--- src/test/java/org/testobject/appium/sauceconnect/AndroidTest.java
+++ src/test/java/org/testobject/appium/sauceconnect/AndroidTest.java
@@ -1,11 +1,28 @@
 package org.testobject.appium.sauceconnect;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AndroidTest extends AbstractTest {
 	@BeforeEach
 	void setUp() {
 		String apiKey = getEnv("TESTOBJECT_API_KEY_ANDROID");
 		initAndroidDriver(apiKey);
+	}
+
+	@Test
+	public void test() {
+		String endpoint = getEnv("DESTINATION_URL");
+		driver.findElementById("endpoint").sendKeys(endpoint);
+		driver.findElementById("button").click();
+		String responseStatus = driver.findElementById("response_status").getText();
+		String content = driver.findElementById("content").getText();
+
+		System.out.println("Response: " + responseStatus);
+		System.out.println(content);
+
+		assertEquals(200, Integer.parseInt(responseStatus));
 	}
 }
